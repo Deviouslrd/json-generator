44e1315986deed0ba60b55d11e7f00cdc5866ad2
Deviouslrd <63770389+Deviouslrd@users.noreply.github.com>
2021-01-12 21:19:58 -0700

    Stuff for tomorrow, save.js update

--- functions/save.js
+++ functions/save.js
@@ -6,26 +6,19 @@ document.getElementById("savebutton").addEventListener('click', async () => {
 });
 
 function onLoad() {
-    document.getElementById("blockName").value = localStorage.blockName;
-    document.getElementById("modName").value = localStorage.modName;
-    document.getElementById("saveLocation").value = localStorage.path;
-}
-
-const jsonObject = {
-    ModName: document.getElementById("modName").value,
-    BlockName: document.getElementById("blockName").value
-};
-
-const jsonProduct = JSON.stringify(jsonObject, null, 4);
-
-function switchPage() {
-    fs.writeFile('../values.json', jsonProduct, (err) => {
-        if (err) throw err;
-    });
+    if (localStorage.blockName) {
+        document.getElementById("blockName").value = localStorage.blockName;
+    }
+    if (localStorage.modName) {
+        document.getElementById("modName").value = localStorage.modName;
+    }
+    if (localStorage.path) {
+        document.getElementById("saveLocation").value = localStorage.path;
+    }
 }
 
 document.getElementById("generateBtn").addEventListener('click', () => {
-    fs.writeFile('../values.json', jsonProduct, (err) => {
-        if (err) throw err;
-    });
+    localStorage.blockName = document.getElementById("blockName").value;
+    localStorage.modName = document.getElementById("modName").value;
+    localStorage.path = document.getElementById("saveLocation").value; 
 });
