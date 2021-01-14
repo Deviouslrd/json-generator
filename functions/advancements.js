bcba2f3b88881f03988c39b660d53a733524a887
Deviouslrd <63770389+Deviouslrd@users.noreply.github.com>
2021-01-13 20:47:14 -0700

    Tweaks + advancement functionality.

--- functions/advancements.js
+++ functions/advancements.js
@@ -2,47 +2,65 @@ const fs = require('fs');
 
 document.getElementById("advanceForm").onsubmit = form => {
     form.preventDefault();
+    
+    if (document.getElementById("saveLocation").value === 'No Location') {
+        return document.getElementById("errorholder").innerHTML = `Error: No save location given!`;
+    }
 
     const filepath = localStorage.path;
 
-    var blockName = document.getElementById("blockName").value;
-    var modName = document.getElementById("modName").value;
-
-    localStorage.modName = modName;
-    localStorage.blockName = blockName;
-    
     if (!filepath || localStorage.path === undefined) {
         return console.log('No filepath.');
     } 
 
+    var blockName = document.getElementById("blockName").value;
+
+    localStorage.blockName = blockName;
+
     blockName = blockName.toLowerCase().split(/ +/).join('_');
-    modName = modName.toLowerCase().split(/ +/).join('_');
 
-    if (document.getElementById("").checked === true) {
-
-        const jsonProduct = {
-
-        };
-        
-        const jsonContent = JSON.stringify(jsonProduct, null, 4);
-
-        if (!fs.existsSync(`${filepath}\\advancements`)) {
-            fs.mkdir(`${filepath}\\advancements`, (err) => {
-                if (err) throw err;
-                console.log('Made the  folder.');
-            });
+    const jsonProduct = {
+        parent: `minecraft:recipes/root`,
+        rewards: {
+            recipes: [
+                "ModName:BlockName_slab",
+                "ModName:BlockName_stairs",
+                "ModName:BlockName_wall"
+            ]
+        },
+        criteria: {
+            has_stone: {
+                trigger: `minecraft:inventory_changed`,
+                conditions: {
+                    items: [
+                        {
+                            item: "ModName:BlockName"
+                        }
+                    ]
+                }
+            }
         }
+    };
+    
+    const jsonContent = JSON.stringify(jsonProduct, null, 4);
 
-        fs.writeFile(`${filepath}\\advancements\\${blockName}.json`, jsonContent, 'utf8', (err) => {
+    if (!fs.existsSync(`${filepath}\\advancements`)) {
+        fs.mkdir(`${filepath}\\advancements`, (err) => {
             if (err) throw err;
-            console.log('made file');
-
+            console.log('Made the advancements folder.');
         });
-        
-        document.getElementById("generateBtn").value = "Generated!";
-
-        setTimeout(() => {
-            document.getElementById("generateBtn").value ="Generate!";
-        }, 1000);
     }
-}
+
+    fs.writeFile(`${filepath}\\advancements\\${blockName}.json`, jsonContent, 'utf8', (err) => {
+        if (err) throw err;
+        console.log('made file');
+
+    });
+    
+    document.getElementById("generateBtn").value = "Generated!";
+
+    setTimeout(() => {
+        document.getElementById("generateBtn").value ="Generate!";
+    }, 1000);
+    
+};
