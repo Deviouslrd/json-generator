70e0a7d8d640448b315e2aed3e8b8ee03e306617
Deviouslrd <63770389+Deviouslrd@users.noreply.github.com>
2021-01-13 21:00:45 -0700

    Loot tables should now be working.

--- functions/loot_tables.js
+++ functions/loot_tables.js
@@ -18,8 +18,8 @@ document.getElementById("lootTableForm").onsubmit = form => {
     blockName = blockName.toLowerCase().split(/ +/).join('_');
     modName = modName.toLowerCase().split(/ +/).join('_');
 
-    if (document.getElementById("").checked === true) {
-
+    // Block Creator
+    if (document.getElementById("block").checked === true) {
         const jsonProduct = {
             type: "minecraft:block",
             pools: [
@@ -47,13 +47,148 @@ document.getElementById("lootTableForm").onsubmit = form => {
         fs.writeFile(`${filepath}\\loot_tables\\${blockName}.json`, jsonContent, 'utf8', (err) => {
             if (err) throw err;
             console.log('Made the loot table file');
-
         });
+    }   
+    
+    // Slab Creator
+    if (document.getElementById("slab").checked === true) {
+        const jsonProduct = {
+            type: "minecraft:block",
+            pools: [
+                {
+                    rolls: 1,
+                    entries: [
+                        {
+                            type: "minecraft:item",
+                            name: `${modName}:${blockName}`
+                        }
+                    ]
+                }
+            ]
+        };
         
-        document.getElementById("generateBtn").value = "Generated!";
+        const jsonContent = JSON.stringify(jsonProduct, null, 4);
 
-        setTimeout(() => {
-            document.getElementById("generateBtn").value ="Generate!";
-        }, 1000);
+        if (!fs.existsSync(`${filepath}\\loot_tables`)) {
+            fs.mkdir(`${filepath}\\loot_tables`, (err) => {
+                if (err) throw err;
+                console.log('Made the loot tables folder.');
+            });
+        }
+
+        fs.writeFile(`${filepath}\\loot_tables\\${blockName}_slab.json`, jsonContent, 'utf8', (err) => {
+            if (err) throw err;
+            console.log('Made the loot table file');
+        });
+    }   
+
+    // Stair Creator
+    if (document.getElementById("stairs").checked === true) {
+        const jsonProduct = {
+            type: "minecraft:block",
+            pools: [
+                {
+                    rolls: 1,
+                    entries: [
+                        {
+                            type: "minecraft:item",
+                            name: `${modName}:${blockName}`
+                        }
+                    ]
+                }
+            ]
+        };
+        
+        const jsonContent = JSON.stringify(jsonProduct, null, 4);
+
+        if (!fs.existsSync(`${filepath}\\loot_tables`)) {
+            fs.mkdir(`${filepath}\\loot_tables`, (err) => {
+                if (err) throw err;
+                console.log('Made the loot tables folder.');
+            });
+        }
+
+        fs.writeFile(`${filepath}\\loot_tables\\${blockName}_stairs.json`, jsonContent, 'utf8', (err) => {
+            if (err) throw err;
+            console.log('Made the loot table file');
+        });
+    }   
+    
+    // Wall Creator
+    if (document.getElementById("wall").checked === true) {
+        const jsonProduct = {
+            type: "minecraft:block",
+            pools: [
+                {
+                    rolls: 1,
+                    entries: [
+                        {
+                            type: "minecraft:item",
+                            name: `${modName}:${blockName}`
+                        }
+                    ]
+                }
+            ]
+        };
+        
+        const jsonContent = JSON.stringify(jsonProduct, null, 4);
+
+        if (!fs.existsSync(`${filepath}\\loot_tables`)) {
+            fs.mkdir(`${filepath}\\loot_tables`, (err) => {
+                if (err) throw err;
+                console.log('Made the loot tables folder.');
+            });
+        }
+
+        fs.writeFile(`${filepath}\\loot_tables\\${blockName}_wall.json`, jsonContent, 'utf8', (err) => {
+            if (err) throw err;
+            console.log('Made the loot table file');
+        });
+    }   
+
+    // Pillar Creator
+    if (document.getElementById("pillar").checked === true) {
+        const jsonProduct = {
+            type: "minecraft:block",
+            pools: [
+                {
+                    rolls: 1,
+                    entries: [
+                        {
+                            type: "minecraft:item",
+                            name: `${modName}:${blockName}`
+                        }
+                    ]
+                }
+            ]
+        };
+        
+        const jsonContent = JSON.stringify(jsonProduct, null, 4);
+
+        if (!fs.existsSync(`${filepath}\\loot_tables`)) {
+            fs.mkdir(`${filepath}\\loot_tables`, (err) => {
+                if (err) throw err;
+                console.log('Made the loot tables folder.');
+            });
+        }
+
+        fs.writeFile(`${filepath}\\loot_tables\\${blockName}_pillar.json`, jsonContent, 'utf8', (err) => {
+            if (err) throw err;
+            console.log('Made the loot table file');
+        });
+    }  
+    
+    if (document.getElementById("block").checked === false &&
+    document.getElementById("slab").checked === false &&
+    document.getElementById("stairs").checked === false &&
+    document.getElementById("wall").checked === false &&
+    document.getElementById("pillar").checked === false) {
+        return document.getElementById("errorholder").innerHTML = "Error: No boxes were selected!";
     }
-}
+
+    document.getElementById("generateBtn").value = "Generated!";
+
+    setTimeout(() => {
+        document.getElementById("generateBtn").value ="Generate!";
+    }, 1000);
+};
