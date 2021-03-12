e8052c37d9e759207d2886688235a8b28bb23a28
Deviouslrd <cdehaas985@gmail.com>
2021-03-11 17:09:55 -0700

    electron-updater addition

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 const { app, BrowserWindow, Menu, MenuItem } = require("electron");
+require('update-electron-app')({
+    updateInterval: '1 hour'
+});
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+      type: 'info',
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    };
+  
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+      if (returnValue.response === 0) autoUpdater.quitAndInstall();
+    });
+});
+
+autoUpdater.on('error', message => {
+    console.error('There was a problem updating the application');
+    console.error(message);
+});
 
 function createWindow () {
     const win = new BrowserWindow({
